bed16d3646a517b70fd4027dc56ade9cefe4d7a2
Test Projects 83 <testprojects83@gmail.com>
2021-11-16 10:32:59 +0000

    first commit on Netflix Clone with React

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDYtM1Ukz43IUikKL9RIh4FtDgAvBbGpCA',
+  authDomain: 'netflix-clone-react-45e72.firebaseapp.com',
+  projectId: 'netflix-clone-react-45e72',
+  storageBucket: 'netflix-clone-react-45e72.appspot.com',
+  messagingSenderId: '827538575493',
+  appId: '1:827538575493:web:058ad8719f1f91e79159ec',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
